fd4d7c5895696d8f8321714832f0b0ef24e80a17
butchtabios <butchtabios@live.com>
2014-03-31 13:25:07 +0800

    Added OrcadWnd

---
+++ TBDOSBOX/OrcadWnd.cpp
@@ -0,0 +1,30 @@
+// OrcadWnd.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "TBDOSBOX.h"
+#include "OrcadWnd.h"
+
+
+// COrcadWnd
+
+IMPLEMENT_DYNAMIC(COrcadWnd, CWnd)
+
+COrcadWnd::COrcadWnd()
+{
+
+}
+
+COrcadWnd::~COrcadWnd()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(COrcadWnd, CWnd)
+END_MESSAGE_MAP()
+
+
+
+// COrcadWnd message handlers
+
+
---
+++ TBDOSBOX/OrcadWnd.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// COrcadWnd
+
+class COrcadWnd : public CWnd
+{
+	DECLARE_DYNAMIC(COrcadWnd)
+
+public:
+	COrcadWnd();
+	virtual ~COrcadWnd();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
